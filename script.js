af9b558b8a846484f614b553d72a289a99c8809e
sanjay-kumar-singh-73 <145865352+sanjay-kumar-singh-73@users.noreply.github.com>
2024-09-29 00:36:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var  newtyped = new  Typed(".loop",{
+    strings:["Frontend Developer", "You Tuber", "Backend developer","Full Stack Developer"],
+    typeSpeed:150,
+    backSpeed:150,
+    looped:true
+})
